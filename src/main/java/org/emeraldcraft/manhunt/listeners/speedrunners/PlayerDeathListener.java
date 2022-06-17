55e666855e6665d84841f770dad58249ef66f351
EmeraldWither <bikechamp007@gmail.com>
2022-06-17 17:03:45 -0400

    Detect speedrunner death

--- src/main/java/org/emeraldcraft/manhunt/listeners/speedrunners/PlayerDeathListener.java
+++ src/main/java/org/emeraldcraft/manhunt/listeners/speedrunners/PlayerDeathListener.java
@@ -5,10 +5,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.emeraldcraft.manhunt.Manhunt;
+import org.emeraldcraft.manhunt.entities.players.ManhuntPlayer;
 import org.emeraldcraft.manhunt.entities.players.Speedrunner;
 import org.emeraldcraft.manhunt.events.speedrunner.SpeedrunnerDeathEvent;
 
 import static org.emeraldcraft.manhunt.enums.ManhuntTeam.SPEEDRUNNER;
+import static org.emeraldcraft.manhunt.utils.IManhuntUtils.debug;
 
 public class PlayerDeathListener implements Listener {
     @EventHandler
@@ -16,20 +18,30 @@ public class PlayerDeathListener implements Listener {
         if(Manhunt.getAPI().isRunning()) {
             //Check to see if the player is in the game
             if(Manhunt.getAPI().getPlayer(event.getEntity().getUniqueId()) == null) return;
+            debug("Player that died is in the game >> " + event.getPlayer().getName());
             //Check to see if the player is a speedrunner
             if(!(Manhunt.getAPI().getPlayer(event.getEntity().getUniqueId()) instanceof Speedrunner speedrunner)) return;
+            debug("Player that died is a speedrunner");
             //Call our death event, and cancel the event if our event is cancelled
             SpeedrunnerDeathEvent speedrunnerDeathEvent = new SpeedrunnerDeathEvent(speedrunner, event);
             Bukkit.getPluginManager().callEvent(speedrunnerDeathEvent);
             if(speedrunnerDeathEvent.isCancelled()){
+                debug("Speedrunner death event was cancelled. Player will not die!");
                 event.setCancelled(true);
                 return;
             }
             //Eliminate the player from the game
             speedrunner.eliminate();
-            if(Manhunt.getAPI().getTeam(SPEEDRUNNER).size() == 0){
-                Manhunt.getAPI().end();
+            //Check to see if the game is over
+            boolean hasEnded = true;
+            for (ManhuntPlayer manhuntPlayer : Manhunt.getAPI().getTeam(SPEEDRUNNER)) {
+                if (!((Speedrunner) manhuntPlayer).isEliminated()) {
+                    hasEnded = false;
+                    break;
+                }
             }
+            debug("Detected game end because of all speed runner deaths. ");
+            if(hasEnded) Manhunt.getAPI().end();
         }
     }
 
