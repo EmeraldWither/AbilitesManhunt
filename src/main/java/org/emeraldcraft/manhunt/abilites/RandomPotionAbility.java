abcdf084866fbe91ce1c8a8d7950c4aa5a85c2c7
25sayi01 <25sayi01@EB1030S-650.MAHWAH.LOCAL>
2022-06-20 11:12:23 -0400

    remove copyright thing

--- src/main/java/org/emeraldcraft/manhunt/abilites/RandomPotionAbility.java
+++ src/main/java/org/emeraldcraft/manhunt/abilites/RandomPotionAbility.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2022-2023 Ishaan Sayal 
- *
- * Licensed under the EmeraldCraft License, Version 2.0 (the "Eshan License");
- * you may not use this file except in compliance with the Eshan License.
- * You may obtain a copy of the Eshan License at 
- *
- *      http://www.emeraldcraft.org/licenses/EmeraldCraft-LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the Eshan License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Eshan License for the specific language governing permissions and 
- * limitations under the Eshan License.
- */
 package org.emeraldcraft.manhunt.abilites;
 
 import static org.emeraldcraft.manhunt.Manhunt.getAPI;
